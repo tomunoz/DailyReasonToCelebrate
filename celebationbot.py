25f85049d526f6bda2be7030204db4412f372188
Tom Munoz <tom.o.munoz@gmail.com>
2021-01-16 23:21:32 -0500

    updated for new dailycelebrationbot

--- celebationbot.py
+++ celebationbot.py
@@ -6,7 +6,7 @@ import time
 import os
 from os import environ
 
-print("This is a Twitter bot named 'DailyReasonToCelebrate' 1")
+#print("This is a Twitter bot named 'DailyReasonToCelebrate' 1")
 # credentials to login to twitter api
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
@@ -18,4 +18,12 @@ auth = tp.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tp.API(auth, wait_on_rate_limit=True)
 
-print("This is a Twitter bot named 'DailyReasonToCelebrate'")
+#print("This is a Twitter bot named 'DailyReasonToCelebrate'")
+
+Holidays = {"2021-01-16":"Teachers Day : Thailand"}
+Date_Today = date.today()
+#print("Today's date is ", Date_Today)
+
+#print(Holidays["2021-01-16"])
+
+api.update_status(Holidays["2021-01-16"])
