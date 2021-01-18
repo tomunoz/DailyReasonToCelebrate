58dc5cb028ed58c5d361fe8398dbe80ee4c6da6e
Tom Munoz <tom.o.munoz@gmail.com>
2021-01-17 20:38:38 -0500

    added spreadsheet of holidays and changed logic to read xlsx and format post to Twitter

--- celebrationbot.py
+++ celebrationbot.py
@@ -5,6 +5,7 @@ from datetime import date
 import time
 import os
 from os import environ
+import pandas as pd
 
 #print("This is a Twitter bot named 'DailyReasonToCelebrate' 1")
 # credentials to login to twitter api
@@ -19,17 +20,23 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tp.API(auth, wait_on_rate_limit=True)
 
 #print("This is a Twitter bot named 'DailyReasonToCelebrate'")
-
-Holidays = {"2021-01-16":"Jan 16 : Teachers Day : Thailand",
-            "2021-01-17":"Jan 17 : National Hot Buttered Rum Day : Unknown Location",
-            "2021-01-18":"Jan 18 : Winnie The Pooh Day : Where Winnie The Pooh Is Loved"}
-
 Date_Today = date.today()
+
+Holidays_df = pd.read_excel("DailyHolidays.xlsx")
+today_holidays_df = Holidays_df.loc[Holidays_df["Date"] == str(Date_Today)]
+today_holidays_days = today_holidays_df["Holiday"]
+
+Holidays = ""
+i = 1
+for item in today_holidays_days:
+    Holidays = Holidays + str(i) + "]" + item + ", "
+    i += 1
 
 while True:
     try:
-        api.update_status(Holidays[str(Date_Today)])
+        api.update_status("Today's opportunities to celebrate include: " + Holidays)
+        print("Successful Twitter status update")
     except:
         pass
-    print("Starting new hour.")
+    print("Starting new hour")
     time.sleep(3600)
