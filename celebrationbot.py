236a6baa11341c00aae33e1852161ea4b5b04ff5
Tom Munoz <tom.o.munoz@gmail.com>
2021-01-18 21:11:57 -0500

    trying to fix font issue crashing in heroku

--- celebrationbot.py
+++ celebrationbot.py
@@ -49,10 +49,10 @@ for index, row in today_holidays_df.iterrows():
 img = Image.open(image_in_file)
 draw = ImageDraw.Draw(img)
 #font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', size=10)
-this_font = "arial.ttf"
-font = ImageFont.truetype(font=this_font, size=25)
+#this_font = "arial.ttf"
+font = ImageFont.truetype("arial.ttf", 25)
 draw.text((10, 10), text, (0, 51, 204), font=font)
-draw.text((10, 420), str(Date_Today), (0, 51, 204), font=this_font)
+draw.text((10, 420), str(Date_Today), (0, 51, 204), font=font)
 img.save(image_out_file)
 
 # create the image file with text that will be posted to twitter
